af6f5077458d9ce51cee3c0a45a16fc38cbaa0b6
Hajin-Bang <hajin0619@naver.com>
2024-02-02 19:38:49 +0900

    feat: 폴더 검색 컴포넌트 UI

---
+++ src/components/share/Search/SearchFolder.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import CommonBottomSheet from '../../base/modal/CommonBottomSheet';
+import { NoSearchResult } from './NoSearchResult';
+import SearchInput from '../../base/SearchInput';
+import Card from '../../base/Card';
+import Button from '../../base/Button';
+import { colors } from '../../../styles/colors';
+import Folder from '../../../assets/icons/Folder';
+import styled from 'styled-components';
+
+const SearchFolder = () => {
+  return (
+    <CommonBottomSheet title="폴더 선택" rightButtonType="close">
+      <SearchContainer>
+        <Button
+          width={93}
+          height={36}
+          varient="filled"
+          color="default"
+          text="폴더추가"
+          renderIcon={() => <Folder size={24} />}
+          onPress={() => console.log('Add folder pressed')}
+        />
+        <SearchInput width={241} />
+      </SearchContainer>
+      <FolderListContainer>
+        <Card title="먹고싶은 것" numberOfLinks={340} />
+      </FolderListContainer>
+    </CommonBottomSheet>
+  );
+};
+
+export default SearchFolder;
+
+const SearchContainer = styled(View)`
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+
+const FolderListContainer = styled(View)`
+  padding: 16px 0;
+  background-color: #171719;
+`;
