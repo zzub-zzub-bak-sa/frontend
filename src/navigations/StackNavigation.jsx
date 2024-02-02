d425fbeb16b6320f21edf3a02f2fcc056c504989
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-03 04:14:30 +0900

    feat: 네비게이션에 Gallery 추가

--- src/navigations/StackNavigation.jsx
+++ src/navigations/StackNavigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../pages/HomeScreen';
+import GalleryScreen from '../pages/GalleryScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ const StackNavigation = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Main" component={HomeScreen} />
+      <Stack.Screen name="Gallery" component={GalleryScreen} />
       {/* 다른 screen을 추가 */}
     </Stack.Navigator>
   );
