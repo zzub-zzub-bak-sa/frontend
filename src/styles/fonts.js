9112a0a53db1e585937ecc124044ba94aa3917c8
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-02 14:54:33 +0900

    feat: font 수정

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,102 +1,102 @@
 import { StyleSheet } from 'react-native';
-import font from '../utils/font';
+import size from '../utils/size';
 
 export const title1 = StyleSheet.create({
   bold: {
     fontFamily: 'SUIT-Bold',
-    fontSize: font(25),
+    fontSize: size.width * 25,
   },
 });
 
 export const title2 = StyleSheet.create({
   bold: {
     fontFamily: 'SUIT-Bold',
-    fontSize: font(20),
+    fontSize: size.width * 20,
   },
   medium: {
     fontFamily: 'SUIT-Medium',
-    fontSize: font(20),
+    fontSize: size.width * 20,
   },
 });
 
 export const title3 = StyleSheet.create({
   bold: {
     fontFamily: 'SUIT-Bold',
-    fontSize: font(19),
+    fontSize: size.width * 19,
   },
   semibold: {
     fontFamily: 'SUIT-Semibold',
-    fontSize: font(19),
+    fontSize: size.width * 19,
   },
 });
 
 export const subtitle1 = StyleSheet.create({
   semibold: {
     fontFamily: 'SUIT-Semibold',
-    fontSize: font(17),
+    fontSize: size.width * 17,
   },
 });
 
 export const subtitle2 = StyleSheet.create({
   semibold: {
     fontFamily: 'SUIT-Semibold',
-    fontSize: font(16),
+    fontSize: size.width * 16,
   },
   medium: {
     fontFamily: 'SUIT-Medium',
-    fontSize: font(16),
+    fontSize: size.width * 16,
   },
 });
 
 export const subtitle3 = StyleSheet.create({
   bold: {
     fontFamily: 'SUIT-Bold',
-    fontSize: font(15),
+    fontSize: size.width * 15,
   },
 });
 
 export const body1 = StyleSheet.create({
   medium: {
     fontFamily: 'SUIT-Medium',
-    fontSize: font(16),
+    fontSize: size.width * 16,
   },
 });
 
 export const body2 = StyleSheet.create({
   medium: {
     fontFamily: 'SUIT-Medium',
-    fontSize: font(15),
+    fontSize: size.width * 15,
   },
 });
 
 export const body3 = StyleSheet.create({
   medium: {
     fontFamily: 'SUIT-Medium',
-    fontSize: font(14),
+    fontSize: size.width * 14,
   },
 });
 
 export const caption1 = StyleSheet.create({
   semibold: {
     fontFamily: 'SUIT-Semibold',
-    fontSize: font(13),
+    fontSize: size.width * 13,
   },
   medium: {
     fontFamily: 'medium',
-    fontSize: font(13),
+    fontSize: size.width * 13,
   },
 });
 
 export const caption2 = StyleSheet.create({
   medium: {
     fontFamily: 'SUIT-Medium',
-    fontSize: font(12),
+    fontSize: size.width * 12,
   },
 });
 
 export const caption3 = StyleSheet.create({
   medium: {
     fontFamily: 'SUIT-Medium',
-    fontSize: font(11),
+    fontSize: size.width * 11,
   },
 });
