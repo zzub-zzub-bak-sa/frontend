01f2e62524b3ee849ed573179b0690d4a7178246
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-03 14:20:56 +0900

    feat: 토스트 config 생성 및 불필요한 코드 제거

--- App.jsx
+++ App.jsx
@@ -1,33 +1,72 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { NavigationContainer } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
+import { Platform, Text, TouchableOpacity, View } from 'react-native';
+import styled from 'styled-components/native';
 import StackNavigation from './src/navigations/StackNavigation';
+import size from './src/utils/size';
+import { colors } from './src/styles/colors';
+import { subtitle3 } from './src/styles/fonts';
+
 GestureHandlerRootView;
 
-// const getFont = async () => {
-//   await Font.loadAsync({
-//     semibold: require('./src/assets/fonts/SUIT-SemiBold.ttf'),
-//     medium: require('./src/assets/fonts/SUIT-Medium.ttf'),
-//     bold: require('./src/assets/fonts/SUIT-Bold.ttf'),
-//     extrabold: require('./src/assets/fonts/SUIT-ExtraBold.ttf'),
-//   });
-// };
+const toastConfig = {
+  showSuccess: ({ text1, text2, onPressMove }) => (
+    <SuccessToastContainer>
+      <SuccessToastText>{text1}</SuccessToastText>
+      <TouchableOpacity onPress={onPressMove}>
+        <SuccessToastLink>{text2}</SuccessToastLink>
+      </TouchableOpacity>
+    </SuccessToastContainer>
+  ),
+};
 
-function App() {
-  // useEffect(() => {
-  //   getFont();
-  // }, []);
-
+function App(props) {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BottomSheetModalProvider>
         <NavigationContainer>
           <StackNavigation />
         </NavigationContainer>
+        <Toast config={toastConfig} />
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
 }
 
 export default App;
+
+const SuccessToastContainer = styled(View)`
+  height: ${size.height * 60}px;
+  width: ${size.width * 350}px;
+  background-color: ${colors.grey[200]};
+  padding: 0 ${size.width * 20}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  ${Platform.select({
+    ios: `
+      shadow-color: #000;
+      shadow-offset: 4px 4px;
+      shadow-opacity: 0.08;
+      shadow-radius: 16px;
+    `,
+    android: `
+      elevation: 5;
+    `,
+  })}
+`;
+
+const SuccessToastText = styled(Text)`
+  font-family: ${subtitle3.bold.fontFamily};
+  font-size: ${subtitle3.bold.fontSize}px;
+  color: white;
+`;
+
+const SuccessToastLink = styled(Text)`
+  font-family: ${subtitle3.medium.fontFamily};
+  font-size: ${subtitle3.medium.fontSize}px;
+  color: white;
+`;
