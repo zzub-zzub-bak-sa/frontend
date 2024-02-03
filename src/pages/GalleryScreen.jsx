1d945f0cbbf922e9fb9e4e2cfdec102e64c43ab0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-03 16:39:33 +0900

    feat: 갤러리 스크린 구현

--- src/pages/GalleryScreen.jsx
+++ src/pages/GalleryScreen.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Text, TouchableOpacity, View } from 'react-native';
 import styled from 'styled-components/native';
+import { useNavigation } from '@react-navigation/native';
 import Layout from '../components/layout/Layout';
 import size from '../utils/size';
 import IcBack from '../assets/icons/IcBack';
-import { body2, caption1, title3 } from '../styles/fonts';
+import { body1, body2, caption1, title3 } from '../styles/fonts';
 import IcSearch from '../assets/icons/IcSearch';
 import IcArrowDown from '../assets/icons/IcArrowDown';
 import { colors } from '../styles/colors';
+import CommonShortBottomSheet from '../components/base/modal/CommonShortBottomSheet';
+import Check from '../assets/icons/Check';
+import { WIDTH } from '../constants/constants';
+import Button from '../components/base/Button';
+import CancelOrNotModal from '../components/GalleryScreen/CancelOrNotModal';
+import SearchFolder from '../components/share/Search/SearchFolder';
+import { showSuccessToast } from '../utils/showSuccessToast';
+import CreateFolder from '../components/share/Create/CreateFolder';
+import ChangeImage from '../components/share/Create/ChangeImage';
 
-const GalleryScreenjsx = () => {
+const GalleryScreen = () => {
+  const navigation = useNavigation();
+  const [openEdit, setOpenEdit] = useState(false);
+  const [currentEdit, setCurrentEdit] = useState('');
+  const [selected, setSelected] = useState([]);
+  const [openCancel, setOpenCancel] = useState(false);
+  const [openFolder, setOpenFolder] = useState(false);
+  const [selectedFolder, setSelectedFolder] = useState('');
+  const [showToast, setShowToast] = useState(false);
+  const [openSort, setOpenSort] = useState(false);
+  const [selectedSort, setSelectedSort] = useState('정렬기준');
+  const [createNewFolder, setCreateNewFolder] = useState(false);
+  const [openChangeImage, setOpenChangeImage] = useState(false);
+  const [folderImage, setFolderImage] = useState('');
+
+  useEffect(() => {
+    if (showToast) {
+      showSuccessToast({
+        text1: '이동되었습니다.',
+        text2: '보러가기',
+        onPressMove: () => null,
+      });
+      setShowToast(false);
+    }
+  }, [showToast]);
+
   return (
     <Layout>
       <Header>
@@ -17,31 +52,131 @@ const GalleryScreenjsx = () => {
           <IcBack size={24} color="white" />
           <Title>폴더제목 최대글자수 16자입</Title>
         </TitleBox>
-        <TouchableOpacity>
-          <Edit>편집</Edit>
-        </TouchableOpacity>
+        {currentEdit ? (
+          <TouchableOpacity onPress={() => setOpenCancel(true)}>
+            <Edit>취소</Edit>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={() => setOpenEdit(true)}>
+            <Edit>편집</Edit>
+          </TouchableOpacity>
+        )}
       </Header>
       <Row>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('SearchGallery')}>
           <IcSearch size={20} color="white" />
         </TouchableOpacity>
-        <SortBox>
-          <SortBoxText>정렬기준</SortBoxText>
+        <SortBox onPress={() => setOpenSort(true)}>
+          <SortBoxText>{selectedSort}</SortBoxText>
           <IcArrowDown />
         </SortBox>
       </Row>
       <Grid>
         <FlatList
-          data={Array(20).fill(0)}
-          renderItem={({ _, idx }) => <ArticleBox index={idx} />}
+          data={Array(20)
+            .fill(0)
+            .map((_, i) => i + 1)}
+          renderItem={({ item }) =>
+            selected.length > 0 && selected.includes(item) ? (
+              <SelectedBox onPress={() => setSelected(selected.filter(el => el !== item))}>
+                <CheckBox>
+                  <Check size={24} color="white" />
+                </CheckBox>
+              </SelectedBox>
+            ) : (
+              <ArticleBox
+                item={item}
+                onPress={() => (currentEdit ? setSelected([...selected, item]) : null)}
+              />
+            )
+          }
+          ListFooterComponent={<FooterComponent />}
           numColumns={3}
         />
       </Grid>
+      {openCancel && (
+        <CancelOrNotModal
+          show={openCancel}
+          onClose={() => setOpenCancel(false)}
+          onEndEdit={() => setCurrentEdit('')}
+        />
+      )}
+      {openEdit && (
+        <CommonShortBottomSheet
+          snapPoints={['20.8%']}
+          onSetValue={setCurrentEdit}
+          onClose={() => setOpenEdit(false)}
+          data={['이동', '삭제']}
+        />
+      )}
+      {currentEdit === '이동' && (
+        <Select>
+          <SelectText>이동할 항목을 선택해 주세요.</SelectText>
+          <Button
+            height={36}
+            width={51}
+            text="다음"
+            varient="filled"
+            color="disable"
+            onPress={() => setOpenFolder(true)}
+          />
+        </Select>
+      )}
+      {openFolder && (
+        <SearchFolder
+          selected={selectedFolder}
+          onPressCard={setSelectedFolder}
+          data={[]}
+          onClose={() => {
+            setOpenFolder(false);
+            setCurrentEdit('');
+            setShowToast(true);
+          }}
+          onPressNewFolder={() => {
+            setOpenFolder(false);
+            setCreateNewFolder(true);
+          }}
+        />
+      )}
+      {openSort && (
+        <CommonShortBottomSheet
+          onSetValue={setSelectedSort}
+          onClose={() => setOpenSort(false)}
+          data={['가나다순', '최신순', '오래된순']}
+        />
+      )}
+      {createNewFolder && (
+        <CreateFolder
+          placeholder="폴더의 이름을 지어주세요."
+          onClose={() => {
+            setCreateNewFolder(false);
+            setCurrentEdit('');
+          }}
+          onBack={() => {
+            setCreateNewFolder(false);
+            setOpenFolder(true);
+          }}
+          onChangeImage={() => {
+            setCreateNewFolder(false);
+            setOpenChangeImage(true);
+          }}
+        />
+      )}
+      {openChangeImage && (
+        <ChangeImage
+          folderImage={folderImage}
+          onPressSelect={setFolderImage}
+          onClose={() => {
+            setOpenChangeImage(false);
+            setCurrentEdit('');
+          }}
+        />
+      )}
     </Layout>
   );
 };
 
-export default GalleryScreenjsx;
+export default GalleryScreen;
 
 const Header = styled(View)`
   height: ${size.height * 60}px;
@@ -98,5 +233,38 @@ const ArticleBox = styled(TouchableOpacity)`
   border-radius: 16px;
   background-color: ${colors.bg[200]};
   margin-bottom: ${size.height * 7}px;
-  margin-right: ${({ index }) => (index % 3 !== 1 ? size.width * 7 : 0)}px;
+  margin-right: ${({ item }) => (item % 3 !== 0 ? size.width * 7 : 0)}px;
+`;
+
+const SelectedBox = styled(ArticleBox)`
+  background-color: ${colors.bg.selected};
+  opacity: 0.9;
+`;
+
+const CheckBox = styled(View)`
+  position: absolute;
+  bottom: ${size.height * 8}px;
+  right: ${size.width * 8}px;
+`;
+
+const Select = styled(View)`
+  width: ${WIDTH}px;
+  height: ${size.height * 96}px;
+  background-color: ${colors.grey[200]};
+  position: absolute;
+  bottom: 0;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 ${size.width * 20}px;
+`;
+
+const SelectText = styled(Text)`
+  font-family: ${body1.medium.fontFamily};
+  font-size: ${body1.medium.fontSize}px;
+  color: ${colors.grey[400]};
+`;
+
+const FooterComponent = styled(View)`
+  height: 120px;
 `;
