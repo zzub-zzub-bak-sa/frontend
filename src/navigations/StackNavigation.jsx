5c90bd493eb387b1d384abf4ea80b3259e47a936
Hajin-Bang <hajin0619@naver.com>
2024-02-03 18:23:26 +0900

    feat: Content Navigation 추가

--- src/navigations/StackNavigation.jsx
+++ src/navigations/StackNavigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../pages/HomeScreen';
+import ContentSceen from '../pages/ContentScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,7 +9,7 @@ const StackNavigation = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Main" component={HomeScreen} />
-      {/* 다른 screen을 추가 */}
+      <Stack.Screen name="Content" component={ContentSceen} />
     </Stack.Navigator>
   );
 };
