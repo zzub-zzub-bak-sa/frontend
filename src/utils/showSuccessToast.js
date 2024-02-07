b606f8790bab0b82a7e57b1bb186f4aa338fd5b3
Hajin-Bang <hajin0619@naver.com>
2024-02-07 16:40:11 +0900

    feat: 기존 toast 수정 및 휴지통 toast 추가

--- src/utils/showSuccessToast.js
+++ src/utils/showSuccessToast.js
@@ -1,12 +1,12 @@
 import Toast from 'react-native-toast-message';
 
 // eslint-disable-next-line import/prefer-default-export
-export const showSuccessToast = ({ text1, text2, onPressMove }) => {
+export const showSuccessToast = ({ text1, text2, onPress }) => {
   Toast.show({
     type: 'showSuccess',
     text1,
     text2,
-    onPressMove,
+    onPress,
     position: 'bottom',
     visibilityTime: 5000,
   });
